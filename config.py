5d8ff22c24fab19661fac05f3aced418c6663176
boo <booblegum42@gmail.com>
2022-04-08 11:01:30 -0700

    fix for heroku

--- config.py
+++ config.py
@@ -3,9 +3,16 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
+db_path = os.environ.get('DATABASE_URL')
+
+if db_path is None:
+    db_path = f'sqlite:///{BASE_DIR / "base.db"})'
+else:
+    db_path = db_path.replace("://", "ql://", 1)
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "base.db"}')
+    SQLALCHEMY_DATABASE_URI = db_path
     TEST_DATABASE_URI = f'sqlite:///{BASE_DIR / "test.db"}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Зачем эта настройка: https://flask-sqlalchemy-russian.readthedocs.io/ru/latest/config.html#id2
     DEBUG = True
